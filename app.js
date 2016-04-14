7b757053a598c8ce8bf42ef5d2da9c6b2cf4f74f
spreadzp <spread2009@gmail.com>
2016-04-14 14:11:04 +0400

    Added files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+
+
+module.exports = function(db){
+
+var express = require('express') ;
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var token = require('./routes/token') ;
+var user = require('./routes/user');
+var authenticate = require('./routes/authenticate');
+var venue = require('./routes/venue');
+var music        = require('./routes/music');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//app.use('/', token);
+app.use('/user', user );
+app.use('/venue', venue);
+app.use('/authenticate', authenticate);
+app.use('/venue/get-music-genres', music);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+return app;
+
+}
+
+
